5b4dd7368dce0e132e80ed8cdceb23007d74df5d
Saki Iwaki <sakiiwaki@SakinoMacBook-Air.local>
2025-04-12 22:48:54 +0900

    MUIのメニューバーをyamadaUIに変更

--- app/react-app-ocr/src/components/menuAppBar.tsx
+++ app/react-app-ocr/src/components/menuAppBar.tsx
@@ -1,95 +1,39 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import {
+  Box,
+  Flex,
+  IconButton,
+  Text,
+  useColorModeValue,
+} from '@yamada-ui/react';
+import { FaBars} from 'react-icons/fa';
 
 export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? 'Logout' : 'Login'}
+    <Box bg={useColorModeValue('blue.500', 'blue.800')} p="4">
+      {/* アプリバー */}
+      <Flex
+        align="center"
+        justify="space-between"
+        bg={useColorModeValue('blue.600', 'blue.900')}
+        p="4"
+        borderRadius="md"
+      >
+        {/* メニューボタン */}
+        <IconButton
+          aria-label="menu"
+          icon={<FaBars />}
+          size="lg"
+          colorScheme="whiteAlpha"
         />
-      </FormGroup>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            読み取るさん
-          </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>履歴</MenuItem>
-                <MenuItem onClick={handleClose}>お気に入り</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
+
+
+        {/* タイトル */}
+        <Text fontSize="xl" fontWeight="bold" color="white">
+          読み取るさん
+        </Text>
+
+      </Flex>
     </Box>
   );
 }
