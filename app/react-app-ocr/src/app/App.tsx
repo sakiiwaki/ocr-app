9176636c68ff90529e8d858e634231ab941a645c
sakiiwaki <saki.lemonn@gmail.com>
2025-04-15 23:23:32 +0900

    フォルダ整理とエイリアス設定

--- app/react-app-ocr/src/app/App.tsx
+++ app/react-app-ocr/src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route} from 'react-router-dom';
-import Home from './pages/Home.tsx';
+import Home from '@app/pages/Home.tsx';
 import { UIProvider } from "@yamada-ui/react"
 
 const App: React.FC = () => {
